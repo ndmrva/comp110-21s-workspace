6c46ee41f5cf0f71d3ffc318f2818531f22e772b
Your Name <you@example.com>
2021-02-06 17:37:13 -0500

    back up test

---
+++ projects/quiz.py
@@ -0,0 +1,21 @@
+"""weird"""
+gme: float = 30.0
+print("0/13: " + str(gme))
+fund_short: float = gme
+rs: float = gme
+
+gme = gme + 130.0
+print("1/26: " + str(gme))
+print("fund: " + str(fund_short - gme))
+print("reddit: " + str(gme -rs))
+
+gme = 200 + gme
+print("01/27: " + str(gme))
+print("fund: " + str(fund_short - gme))
+print(str(gme - rs))
+
+c: str = "zyxyz"
+len("zyxyz")
+print(c[n-1])
+
+
